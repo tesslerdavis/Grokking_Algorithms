cf0f8447e886d80e00bdc3e64ff3d7f8f791aa4d
tesslerdavis <tesslerdaviswork@gmail.com>
2018-01-28 17:52:34 +0900

    adds queue and populates hash table

--- breadth-first.rkt
+++ breadth-first.rkt
@@ -1,29 +1,27 @@
 #lang racket
 
-;super very basic proof of concept that it's possible to make a hash table
-;;and search through lists assigned to keys by degree of seperation.
-
 ;makes hash table
 (define ht (make-hash))
 
 ;adds to it
 (hash-set! ht "me" '("alice" "bob" "claire"))
-(hash-set! ht "alice" '("peggy" "jon" "fred"))
+(hash-set! ht "alice" '("peggy"))
+(hash-set! ht "peggy" '())
+(hash-set! ht "bob" '("jim"))
+(hash-set! ht "claire" '())
+
 
 ;;takes a hash table, start nod, and search nod.
 ;;goes through nod neighbors before moving to friends of friends.
-(define (by-degrees hashname first-nod value-to-find)
-  (let ([nod-neighbors(hash-ref hashname first-nod)])
-    (letrec ([f(lambda (lst)
-                (cond [(null? lst) (by-degrees hashname (car nod-neighbors) value-to-find)] 
-                      [(equal? (car lst) value-to-find) "found a match"]
-                      [#t (f (cdr lst))]))])
-           (f nod-neighbors))))
 
-
-#|TO DO
- - (much later) use lists of pairs with bool flags to skip repeats
- - have an actual queue
- - fix how the null? branch only allows for a two-key hash because it can't move down the nod-neighbors
-   list (which having a real queue will solve.
-|#
+(define (by-degrees hashname first-nod nod-to-find)
+  (let ([que (hash-ref hashname first-nod)])
+    (letrec ([f(lambda (que)
+                 (cond [(null? que)
+                        "does not exist or outside friends network"]
+                       [(equal? (car que) nod-to-find)
+                        "exists inside friends network"]
+                       [#t (begin
+                             (set! que (append que (hash-ref hashname (car que)))) ;;failed matches add their friends to the que
+                                          (f (cdr que)))]))])
+(f que))))
